3327a033c9ab2c61193f4a641a9c4b288f3b0fd4
johndillinger15 <dillinger.stefan@googlemail.com>
2024-01-20 23:16:29 +0100

    added README

--- python_script.py
+++ python_script.py
@@ -7,7 +7,7 @@ from datetime import datetime
 imported_peaks_data = pd.read_csv("peaks_data.csv")
 
 # Define color palette
-color_palette = {"old": "red", "new": "navy", "planned": "purple"}
+color_palette = {"old": "navy", "new": "red", "planned": "purple"}
 
 # Create folium map
 # map_center = [48.9, 12.9]
@@ -31,6 +31,5 @@ map_peaks.fit_bounds(map_peaks.get_bounds())
 
 # Display the map
 map_peaks
-current_date = datetime.now().strftime("%Y%m%d")
-file_name_html = f"peaks_progress_{current_date}.html"
+file_name_html = f"peaks_progress.html"
 map_peaks.save(file_name_html)
